7c063e5790149f9e61dc95f1288a3a9f6143a145
aryan111354 <aryan.patel@edu.sait.ca>
2023-11-21 08:26:11 -0700

    week10

--- app/page.js
+++ app/page.js
@@ -12,7 +12,10 @@ const Page = () => {
       <Link href="/week3">Go to Week 3</Link>
       <Link href="/week4">Go to Week 4</Link>   
       <Link href="/week5">Go to Week 5</Link>  
-      <Link href="/week6">Go to Week 6</Link> 
+      <Link href="/week6">Go to Week 6</Link>
+      <Link href="/week8">Go to Week 8</Link>
+      <Link href="/week10">Go to Week 10</Link>
+
     </div>
   );
 };
---
+++ app/week10/page.js
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NewItem from './new-item';
+import ItemList from './item-list';
+import MealIdeas from './meal-ideas';
+import { useUserAuth } from '../_utils/auth-context';
+import itemsData from './items.json';
+
+const Page = () => {
+  const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const { user } = useUserAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    } else {
+      setIsLoading(false); 
+    }
+  }, [user, navigate]);
+
+
+  const handleAddItem = (newItem) => {
+    setItems(currentItems => [...currentItems, newItem]);
+  };
+
+
+  const handleItemSelect = (itemName) => {
+    setSelectedItemName(itemName);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>; 
+  }
+
+  return (
+    <main className="bg-black-100 p-4 flex">
+      <div>
+        <h1 className="text-2xl font-bold mb-4 text-indigo-400 flex-auto items-center">Shopping List</h1>
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items} onItemSelect={handleItemSelect} />
+      </div>
+      <MealIdeas ingredient={selectedItemName} />
+    </main>
+  );
+};
+
+export default Page;
